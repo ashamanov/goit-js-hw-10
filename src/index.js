ca1d96bc7473451bc5e584d139c7374f9cbccbac
ashamanov <ashamanov.m@gmail.com>
2022-10-29 17:04:31 +0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const ul = document.querySelector('country-list');
+const div = document.querySelector('country-info');
